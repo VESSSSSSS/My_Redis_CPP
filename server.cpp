766fa2ecb2808832daeef6665eafccc1d3c46012
Your Name <2544334701@qq.com>
2023-12-17 03:29:19 +0800

    First commit

---
+++ server.cpp
@@ -0,0 +1,14 @@
+#include <stdint.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include <errno.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+#include <netinet/ip.h>
+
+int main() {
+    int fd = socket(AF_INET, SOCK_STREAM, 0);
+    
+}
